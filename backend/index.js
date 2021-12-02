94b1a8042fd81084380777c902ddde36fba5f10c
unknown <t9miel00@students.oamk.fi>
2021-12-02 19:48:00 +0200

    Image recieved in the frontend

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const fs = require('fs')
 const express = require('express');
 const mqtt = require('mqtt');
 const cors = require('cors');
+const mongoose = require('mongoose')
+
+
 
 
 
@@ -11,6 +14,12 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+
+
+mongoose.connect(process.env.MONGODB_URL);
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 
 const client = mqtt.connect(process.env.CLIENT, { 
@@ -25,8 +34,9 @@ const client = mqtt.connect(process.env.CLIENT, {
   client.subscribe("image") 
   client.on("message", function (topic, message) {
     message = JSON.parse(message.toString()); 
-        var image = Buffer.from(message.data.image, 'utf8');
-        var filename = 'image.jpg';
+        const image = Buffer.from(message.data.image, 'utf8');
+        const id = message.data.id
+        const filename = 'image.jpg';
         fs.writeFile(__dirname + '/image/' + filename, image, { encoding: 'binary' }, function(err) {
             if (err) {
                 console.log('save failed', err);
@@ -34,11 +44,38 @@ const client = mqtt.connect(process.env.CLIENT, {
                 console.log('saved');
             }
         });
+
+
+        delete message.data.image;
+        message.data.filename = filename;
         
-  
+
+
+
    })
 
 
+      app.get('/images', function(req, res, next) {
+        const filename = 'image.jpg';
+        const streamDir = __dirname + '/image/';
+        const img = streamDir + filename;
+        fs.exists(img, function(exists) {
+        	if (exists) {
+                return res.sendFile(img);
+            } else {
+                
+                return res.json({message:"nothing to send"})
+            }
+        });
+    });
+
+
+    app.get('/picture', function(req, res) {
+    client.publish("start", JSON.stringify("lol"));
+    res.json({ message: "works" })
+    console.log("hey!")
+    })
+
 app.listen(8000, function() {
-    console.log("Server listen on port 3001")
+    console.log("Server listen on port 8000")
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [image, setImage] = useState("");
+
+  useEffect(() => {
+    fetchImage();
+  }, []);
+
+  function takePhoto() {
+    fetch("http://localhost:8000/picture");
+  }
+
+  function fetchImage() {
+    fetch("http://localhost:8000/images").then((data) =>
+      setImage(`${data.url}?${new Date().getTime()}`)
+    );
+  }
+
   return (
-    <div >
-      <header >
-        <p>
-       cool
-        </p>
-        
+    <div>
+      <header className="center">
+        <h1>Picture from raspberry</h1>
       </header>
+      <div className="center"></div>
+      <div className="center">
+        <img src={image} alt="Images from raspberry" />
+      </div>
+      <div className="center">
+        <button onClick={takePhoto}> Hey hey hey take photo</button>
+        <button onClick={fetchImage}> Hey hey hey see Photo</button>
+      </div>
     </div>
   );
 }
