15f043696472ada25aac66e75eb93d9988bf51fa
unknown <t9miel00@students.oamk.fi>
2021-12-17 11:27:34 +0200

    Brushed up things

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   const [image, setImage] = useState("");
   const [messages, setMessage] = useState("");
   const [hidden,setHidden] = useState(false)
-
+  const img = document.getElementById("img") as HTMLImageElement;
   useEffect(() => {
     fetchImage();
   }, []);
@@ -75,7 +75,6 @@ function App() {
 
   async function faceDetection() {
     const model = await blazeface.load();
-    const img = document.getElementById("img") as HTMLImageElement;
     const canvas = document.getElementById("canvas") as HTMLCanvasElement
     const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
     const returnTensors = false; 
@@ -88,7 +87,7 @@ function App() {
       const end : any = predictions[i].bottomRight;
       const size = [end[0] - start[0], end[1] - start[1]];
  
-      ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
+      ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
       ctx.fillRect(start[0], start[1], size[0], size[1]);
       setMessage("Face detected")
     }
@@ -110,13 +109,12 @@ function App() {
       </header>
       <div ></div>
       <div className="center">
-        <img  src={image} id="img"  className="imageStyle"   alt="Images from raspberry"  crossOrigin='anonymous'  />  
-       { hidden && <canvas  id="canvas"  width={640} height={480}/>}
+       { !hidden ? <img  src={image} id="img"  className="imageStyle2"  alt="Images from raspberry"  crossOrigin='anonymous'  /> : <canvas  id="canvas"  className="imageStyle2" width={640} height={480}/> }
       </div>
       <div className="center"><p>{messages}</p></div>
       <div className="center" >
         <button  className="button button2" onClick={takePhoto}> take photo</button>
-        <button className="button button2" onClick={fetchImage}>  see Photo</button>
+        <button className="button button2" onClick={() => { setHidden(false); fetchImage();}}>  see Photo</button>
         <button className="button button2" onClick={take60s}>  Take photo every 60s</button>
         <button  className="button button2"onClick={stop60s}>  stop</button>
         <button  className="button button2" onClick={() => { setHidden(true); faceDetection();}}>  Face detection</button>
